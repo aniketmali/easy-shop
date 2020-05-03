db9fb75b2a76fe1166ed841cdd54504142a261ce
aniketmali <aniketmali7@gmail.com>
2020-05-03 17:27:39 +0300

    Added header component with navigation

--- src/pages/shoppage/shoppage.component.jsx
+++ src/pages/shoppage/shoppage.component.jsx
@@ -3,22 +3,20 @@ import CollectionPreview from '../../components/collection-preview/collection-pr
 import SHOP_DATA from './shop.data';
 
 class ShopPage extends React.Component {
-    constructor(props)
-    {
+    constructor(props) {
         super(props);
         this.state = {
-            collections:SHOP_DATA
+            collections: SHOP_DATA
         }
     }
 
-    render()
-    {
-        const {collections} = this.state;
+    render() {
+        const { collections } = this.state;
         return (
             <div className='shop-page'>
                 {
-                    collections.map(({id,...otherCollectionProps}) => (
-                        <CollectionPreview key={id} {...otherCollectionProps}/>
+                    collections.map(({ id, ...otherCollectionProps }) => (
+                        <CollectionPreview key={id} {...otherCollectionProps} />
                     ))
                 }
             </div>
